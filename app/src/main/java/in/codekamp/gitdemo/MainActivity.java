9ecc1100f904ba30ef49d4918209477249cf17d8
Prashant Chaudhary <101.prashant@gmail.com>
2016-04-11 18:43:54 +0530

    initial commit

---
+++ app/src/main/java/in/codekamp/gitdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package in.codekamp.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+//  git  config  --global  user.email  "<your_email_addr>"
+//  git  config  --global  user.name   "<your_name>"
+
+//  git  config  --global  user.email
+//  git  config  --global  user.name
+
+
+//  git init
+
+
+
+
+// git status
+// git add <file_path>       OR    git add -A
+// git commit -m "<message goes here>"
